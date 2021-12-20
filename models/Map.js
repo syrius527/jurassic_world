9669698400db06c3e4270112408e51c970e52feb
syrius527 <djlee1031@naver.com>
2021-12-20 17:47:31 +0900

    create Map.js

---
+++ models/Map.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    player: { type: Schema.Types.ObjectId, ref: 'Player', unique: true },
+    fields: [
+        {
+            x: Number,
+            y: Number,
+            descriptions: Array,
+            canGo: Array,
+            events: Array,
+        },
+    ],
+});
+
+const Map = mongoose.model('map', schema);
+
+module.exports = {
+    Map,
+};
